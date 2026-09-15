458ee508cd722f3a2424de582994013bdaf1cd96
andikariski717-crypto <andikariski717@gmail.com>
2026-09-15 16:11:00 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("data-container");
+const input = document.getElementById("searchInput");
+const hasil = document.getElementById("searchResult");
 
-  
-  if (typeof dataMahasiswa !== "undefined") {
-    let htmlContent = `
-      <table border="1" cellpadding="8" cellspacing="0">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Nama Mahasiswa</th>
-            <th>Nilai</th>
-          </tr>
-        </thead>
-        <tbody>
-    `;
+input.addEventListener("input", function () {
 
-    dataMahasiswa.forEach((mahasiswa, index) => {
-      htmlContent += `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${mahasiswa.nama}</td>
-          <td>${mahasiswa.nilai}</td>
-        </tr>
-      `;
+    const kata = input.value.toLowerCase().trim();
+
+    hasil.innerHTML = "";
+
+    if (kata === "") {
+        return;
+    }
+
+    dataMahasiswa.forEach(function (mahasiswa) {
+
+        const namaMahasiswa = mahasiswa.nama.toLowerCase();
+
+        if (namaMahasiswa.includes(kata)) {
+
+            const item = document.createElement("div");
+
+            item.innerHTML = mahasiswa.nama;
+
+            item.style.padding = "10px";
+            item.style.border = "1px solid black";
+            item.style.backgroundColor = "white";
+            item.style.cursor = "pointer";
+
+            item.addEventListener("click", function () {
+
+                input.value = mahasiswa.nama;
+
+                hasil.innerHTML = `
+                     <div style="padding:10px; border:1px solid black; background:white;">
+                      <h3>${mahasiswa.nama}</h3>
+                      <p>Nilai: ${mahasiswa.nilai}</p>
+                     </div>
+                `;
+            });
+
+            hasil.appendChild(item);
+        }
     });
-
-    htmlContent += `
-        </tbody>
-      </table>
-    `;
-
-    container.innerHTML = htmlContent;
-  } else {
-    container.innerHTML = "<p>Data mahasiswa tidak ditemukan.</p>";
-  }
 });
